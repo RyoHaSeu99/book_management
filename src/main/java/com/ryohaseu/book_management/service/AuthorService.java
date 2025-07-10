6a6d9bc8d73858d149033068a5de1a2c482923a7
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-10 16:11:57 +0900

    first commit

---
+++ src/main/java/com/ryohaseu/book_management/service/AuthorService.java
@@ -0,0 +1,16 @@
+package com.ryohaseu.book_management.service;
+
+import com.ryohaseu.book_management.model.Author;
+import com.ryohaseu.book_management.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public Author create(Author author) {
+        return authorRepository.save(author);
+    }
+}
